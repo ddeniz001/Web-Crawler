18723148143509407d3e9812a7dd0e5f1d98add3
ddeniz001 <38838004+ddeniz001@users.noreply.github.com>
2018-11-14 17:40:31 -0500

    New function for general.py
    set_to_file() Each set item becomes a new line in queued/crawled file

--- Crawler/general.py
+++ Crawler/general.py
@@ -35,10 +35,19 @@ def del_file_contents(path):
         pass
 
 
-# Converts each line in a file to set items
+# Converts each line in a file to set items (Faster approach)
 def file_to_set(file_name):
         results = set()
         with open(file_name, "rt") as f:
-                for line in f:
-                        results.add(line.replace("\n", ""))
-        return results
+                for line in f: # Loops through each line
+                        results.add(line.replace("\n", "")) # Reads url
+        return results # Returns as set
+
+
+# Iterate through a set, each item is a new line in the file 
+def set_to_file(links, file):
+        del_file_contents(file) # All up-to-date data will be stored in links set
+        for link in sorted(links):
+                append_to_file(file, link)
+
+                
