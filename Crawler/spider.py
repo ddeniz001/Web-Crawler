cbb6cfc64c551641bd05193d5851e83862f05de1
ddeniz001 <38838004+ddeniz001@users.noreply.github.com>
2018-11-16 18:14:20 -0500

    New functionality for the spider
    Spider is able to add links to queue file
    Spider can now update both text files

--- Crawler/spider.py
+++ Crawler/spider.py
@@ -33,7 +33,36 @@ class Spider:
         if page_url not in Spider.crawled:
             print(thread_name + "now crawling" + page_url)
             print("Queue: " + str(len(Spider.queue)) + "| Crawled: " + str(len(Spider.crawled))) # Length of queue/crawled files
-            # Spider.add_links_to_queue(Spider.gather_link(page_url))
+            # Spider.add_links_to_queue(Spider.gather_links(page_url))
             # Spider.queue.remove(page_url)
             # Spider.crawled.add(page_url)
-                                                            
+
+    # Converts bytes into string to pass on to LinkFinder                                      
+    def gather_links(page_url):
+        html_string = ""
+        try:
+            response = urlopen(page_url)
+            if response.getheader("Content-type") == "utf-8":
+                html_bytes = response.read()
+                html_string = html_bytes.decode("utf-8") # html_string can be passed onto LinkFinder
+            finder = LinkFinder(Spider.base_url, Spider.page_url) 
+            finder.feed(html_string) # All links can be returned as strings
+        except:
+            print("Sorry, cannot crawl page :(")
+            return set()
+        return finder.page_links() # Passing finder in page_link in order to return links
+
+
+    def add_links_to_queue(links):
+        for url in links:
+            if url in Spider.queue:
+                continue
+            if url in Spider.crawled:
+                continue
+            if Spider.domain_name not in url:
+                continue 
+            Spider.queue.add(url)
+
+    def update_files():
+        set_to_file(Spider.queue, Spider.queue_file)
+        file_to_set(Spider.crawled, Spider.crawled_file)
